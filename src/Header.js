2219c09056761cae12e167085977bd03206518b2
Satvik Sharma <satvik.sharma2@gmail.com>
2021-06-13 12:36:34 +1000

    Tweak header, hopefully more acessible

--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,25 @@
 import React from "react";
 
+const NavLink = ({ children, href, onClick }) => (
+    <li className="nav-link">
+        <a href={href} onClick={onClick}>
+            {children}
+        </a>
+    </li>
+)
+
 export default function Header({ setActive }) {
     const onClick = (section) => () => setActive(section);
-
+  
     return (
-        <section className="header">
-            <ul>
-                <li><a href="#styles" onClick={onClick("styles")}>Styles</a></li>
-                <li><a href="#components" onClick={onClick("components")}>Components</a></li>
-                <li><a href="#desktop" onClick={onClick("desktop")}>Desktop</a></li>
-            </ul>
-        </section>
+        <header className="app-header">
+            <nav>
+                <ul className="nav-list" title="Navigation">
+                    <NavLink href="#styles" onClick={onClick("styles")}>Styles</NavLink>
+                    <NavLink href="#components" onClick={onClick("components")}>Components</NavLink>
+                    <NavLink className="header-link" href="#desktop" onClick={onClick("desktop")}>Desktop</NavLink>
+                </ul>
+            </nav>
+        </header>
     )
 }
