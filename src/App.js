8aeb7a8ccbf28293183379db29011e98da490326
Satvik Sharma <satvik.sharma2@gmail.com>
2021-06-13 12:39:56 +1000

    Change style editor to only work for the editable style tag

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Header from "./Header";
 import ComponentEditor from "./platform/components/ComponentEditor";
 import StyleEditor from "./platform/styles";
 import AppRenderer from './platform/AppRenderer';
-import { initializeState } from "./state";
+import { initializeState, useStore } from "./state";
+
+const useSyncStyles = () => {
+    const css = useStore(store => store.css);
+
+    useEffect(() => {
+        const node = document.getElementById("user-css");
+        if (node) {
+            node.textContent = css;
+        }
+    }, [css]);
+}
 
 function App() {
     const [activeView, setActiveView] = useState("components");
+    useSyncStyles();
 
     return (
         <>
--- src/platform/styles/index.js
+++ src/platform/styles/index.js
@@ -2,66 +2,28 @@ import React, { useEffect, useRef, useState } from "react";
 import { highlight, languages } from 'prismjs/components/prism-core';
 
 import Editor from "../Editor";
+import { useStore }  from "../../state";
 
-const IDS = [
-    "utility-css",
-    "app-css",
-    "user-css",
-];
+const useEditStyles = () => {
+    const [savedCss, saveCss] = useStore(store => [
+        store.css,
+        store.setCss
+    ]);
 
-const useStyleSheet = (id) => {
-    const node = useRef(null);
-    const [css, setCss] = useState();
-    const [readOnly, setReadOnly] = useState(true);
-
-
-    useEffect(() => {
-        const init = async () => {
-            const foundNode = document.getElementById(id);
-            node.current = foundNode;
-            if (foundNode.href) {
-                const res = await fetch(foundNode.href);
-                const fetchedCss = await res.text();
-                console.log(fetchedCss);
-                setCss(fetchedCss);
-                setReadOnly(true);
-            } else if ("innerText" in foundNode) {
-                console.log(foundNode.innerText);
-                setCss(foundNode.innerText);
-                setReadOnly(false);
-            }
-        }
-
-        if (id) {
-            init();
-        }
-    }, [id]);
-
-
-    if (readOnly) {
-        return [css];
-    } else {
-        const save = () => {
-            if (node.current && !readOnly) {
-                node.current.textContent = css;
-            }
-        }
-
-        return [
-            css,
-            setCss,
-            save,
-        ]
+    const [localCss, setLocalCss] = useState(savedCss);
+    const save = () => {
+        saveCss(localCss);
     }
 
+    return [
+        localCss,
+        setLocalCss,
+        save,
+    ];
 }
 
-export default function StyleEditor ({
-
-}) {
-    const [id, setId] = useState(IDS[2]);
-    console.log(id);
-    const [css, setCss, save] = useStyleSheet(id);
+export default function StyleEditor ({ }) {
+    const [css, setCss, save] = useEditStyles();
 
     if (typeof css !== 'string') {
         return <pre>Loading...</pre>
@@ -71,13 +33,17 @@ export default function StyleEditor ({
         <button type="submit">Save</button>
     );
 
+    const onSubmit = e => {
+        e.preventDefault();
+        save();
+    };
+
     return (
         <Editor
             autoFocus
             footer={footer}
             highlight={code => highlight(code, languages.css)}
-            id={id}
-            onSubmit={save}
+            onSubmit={onSubmit}
             onValueChange={setCss}
             padding={10}
             value={css}
